a27735c8a09d0fffdde821b3206f02213eae1ce5
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-11-12 02:02:41 +0330

    Upgrade router to use go1.22 new features

--- router.go
+++ router.go
@@ -1,6 +1,7 @@
 package grape
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -13,28 +14,23 @@ func NewRouter() *Router {
 }
 
 func (r *Router) Get(route string, handler http.HandlerFunc) {
-	//r.Handle(fmt.Sprintf("%s %s", http.MethodGet, route), handler)
-	r.Handle(route, checkMethod(http.MethodGet, handler))
+	r.Handle(fmt.Sprintf("%s %s", http.MethodGet, route), handler)
 }
 
 func (r *Router) Post(route string, handler http.HandlerFunc) {
-	//r.Handle(fmt.Sprintf("%s %s", http.MethodPost, route), handler)
-	r.Handle(route, checkMethod(http.MethodPost, handler))
+	r.Handle(fmt.Sprintf("%s %s", http.MethodPost, route), handler)
 }
 
 func (r *Router) Put(route string, handler http.HandlerFunc) {
-	//r.Handle(fmt.Sprintf("%s %s", http.MethodPut, route), handler)
-	r.Handle(route, checkMethod(http.MethodPut, handler))
+	r.Handle(fmt.Sprintf("%s %s", http.MethodPut, route), handler)
 }
 
 func (r *Router) Patch(route string, handler http.HandlerFunc) {
-	//r.Handle(fmt.Sprintf("%s %s", http.MethodPatch, route), handler)
-	r.Handle(route, checkMethod(http.MethodPatch, handler))
+	r.Handle(fmt.Sprintf("%s %s", http.MethodPatch, route), handler)
 }
 
 func (r *Router) Delete(route string, handler http.HandlerFunc) {
-	//r.Handle(fmt.Sprintf("%s %s", http.MethodDelete, route), handler)
-	http.Handle(route, checkMethod(http.MethodDelete, handler))
+	r.Handle(fmt.Sprintf("%s %s", http.MethodDelete, route), handler)
 }
 
 func (r *Router) Use(middlewares ...func(http.Handler) http.Handler) {
@@ -45,15 +41,4 @@ func (r *Router) Use(middlewares ...func(http.Handler) http.Handler) {
 
 func (r *Router) Serve(addr string) error {
 	return http.ListenAndServe(addr, r)
-}
-
-func checkMethod(method string, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != method {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
 }
