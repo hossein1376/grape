778400b932c8caad2855923f0418d6ef0036e4db
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-11-13 19:28:48 +0330

    Document exported router functions

--- router.go
+++ router.go
@@ -10,6 +10,8 @@ type Router struct {
 	*http.ServeMux
 }
 
+// NewRouter will initialize a router of type Router which embeds type ServeMux,
+// with added helper functions such ad Get, Post, Use and ...
 func NewRouter() *Router {
 	return &Router{ServeMux: http.NewServeMux()}
 }
@@ -38,15 +40,19 @@ func (r *Router) handle(method, route string, handler http.HandlerFunc) {
 	r.Handle(method+" "+route, r.withMiddlewares(handler))
 }
 
+// Use add middlewares to the routes that are defined after it.
+// Note that declared middlewares won't be applied to the previous routes or the default handlers such as NotFound or MethodNotAllowed.
 func (r *Router) Use(middlewares ...func(http.Handler) http.Handler) {
 	r.local = append(r.local, middlewares...)
 }
 
+// UseAll will add declared middleware to all the handlers.
+// No matter defined before or after it; as well as the default handlers such as NotFound or MethodNotAllowed.
 func (r *Router) UseAll(middlewares ...func(http.Handler) http.Handler) {
 	r.global = append(r.global, middlewares...)
 }
 
-func (r *Router) Serve(addr string) error {
+// Serve will start the server on the provided address. It takes an optional argument to modify the server's configurations.
 func (r *Router) Serve(addr string, server ...*http.Server) error {
 	var h http.Handler = r
 	for _, middleware := range r.global {
