3200a48fdda1fc5473bb1f272e9a9e0c3b660d08
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-11-15 17:21:17 +0330

    Create examples directory

---
+++ _examples/simple_server/go.mod
@@ -0,0 +1,7 @@
+module simple_server
+
+go 1.22
+
+replace github.com/hossein1376/grape => ../../
+
+require github.com/hossein1376/grape v0.0.0-00010101000000-000000000000
---
+++ _examples/simple_server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/hossein1376/grape"
+)
+
+type handler struct {
+	// data/models
+	// settings
+	*grape.Server
+}
+
+func main() {
+	h := handler{Server: grape.New()}
+	r := grape.NewRouter()
+
+	r.UseAll(h.LoggerMiddleware, h.RecoverMiddleware)
+	r.Get("/", h.rootHandler)
+	r.Post("/ping", h.pingHandler)
+	r.Put("/{id}", h.parameterHandler)
+
+	h.Info("starting server on port 3000...")
+	err := r.Serve(":3000")
+	if err != nil {
+		h.Error("failed to start server", "error", err)
+		return
+	}
+}
+
+func (h *handler) rootHandler(w http.ResponseWriter, _ *http.Request) {
+	h.Info("Get request on root")
+	h.OkResponse(w, "Hello, World!")
+}
+
+func (h *handler) pingHandler(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Ping string `json:"ping"`
+	}
+
+	var req request
+	err := h.ReadJson(w, r, &req)
+	if err != nil {
+		h.BadRequestResponse(w, err)
+		return
+	}
+
+	h.Info("ping handler", "request", req.Ping)
+	h.NoContentResponse(w)
+}
+
+func (h *handler) parameterHandler(w http.ResponseWriter, r *http.Request) {
+	id := h.ParamInt(r, "id")
+	if id == 0 {
+		h.NotFoundResponse(w)
+		return
+	}
+	h.CreatedResponse(w, fmt.Sprintf("you requested for %d", id))
+}
