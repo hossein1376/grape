0c8aa8e6971d3d881ec4b48127ceffabc5d96700
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-11-15 17:38:19 +0330

    Rename response function

--- response.go
+++ response.go
@@ -17,9 +17,9 @@ type resp struct {
 	Message any `json:"message,omitempty"`
 }
 
-// Respond is a generic function which responses with the provided message and status code,
+// Response is a generic function which responses with the provided message and status code,
 // it will return 500 if case of failure
-func (res *response) Respond(w http.ResponseWriter, statusCode int, message any) {
+func (res *response) Response(w http.ResponseWriter, statusCode int, message any) {
 	err := res.WriteJson(w, statusCode, message, nil)
 	if err != nil {
 		res.logInternalError(err, message)
@@ -29,17 +29,17 @@ func (res *response) Respond(w http.ResponseWriter, statusCode int, message any)
 
 // OkResponse means everything went as expected
 func (res *response) OkResponse(w http.ResponseWriter, data any) {
-	res.Respond(w, http.StatusOK, data)
+	res.Response(w, http.StatusOK, data)
 }
 
 // CreatedResponse indicates that requested resource(s) have been successfully created
 func (res *response) CreatedResponse(w http.ResponseWriter, data any) {
-	res.Respond(w, http.StatusCreated, data)
+	res.Response(w, http.StatusCreated, data)
 }
 
 // NoContentResponse means the operation was successful, and server has nothing more to say about it
 func (res *response) NoContentResponse(w http.ResponseWriter) {
-	res.Respond(w, http.StatusNoContent, nil)
+	res.Response(w, http.StatusNoContent, nil)
 }
 
 // BadRequestResponse indicates that the request has been deemed unacceptable by server
@@ -50,19 +50,19 @@ func (res *response) BadRequestResponse(w http.ResponseWriter, err ...error) {
 	}
 
 	r := resp{Message: msg}
-	res.Respond(w, http.StatusBadRequest, r)
+	res.Response(w, http.StatusBadRequest, r)
 }
 
 // UnauthorizedResponse responds when user is not authorized
 func (res *response) UnauthorizedResponse(w http.ResponseWriter) {
 	r := resp{Message: http.StatusText(http.StatusUnauthorized)}
-	res.Respond(w, http.StatusUnauthorized, r)
+	res.Response(w, http.StatusUnauthorized, r)
 }
 
 // ForbiddenResponse indicates that the action is not allowed
 func (res *response) ForbiddenResponse(w http.ResponseWriter) {
 	r := resp{Message: http.StatusText(http.StatusForbidden)}
-	res.Respond(w, http.StatusForbidden, r)
+	res.Response(w, http.StatusForbidden, r)
 }
 
 // NotFoundResponse will return with classic 404 error message.
@@ -74,13 +74,13 @@ func (res *response) NotFoundResponse(w http.ResponseWriter, err ...error) {
 	}
 
 	r := resp{Message: msg}
-	res.Respond(w, http.StatusNotFound, r)
+	res.Response(w, http.StatusNotFound, r)
 }
 
 // InternalServerErrorResponse indicates something has gone wrong unexpectedly.
 func (res *response) InternalServerErrorResponse(w http.ResponseWriter) {
 	r := resp{Message: http.StatusText(http.StatusInternalServerError)}
-	res.Respond(w, http.StatusInternalServerError, r)
+	res.Response(w, http.StatusInternalServerError, r)
 }
 
 // logInternalError logs the error when writing json response fails
