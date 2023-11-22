59b68e3a99df8614cf7f38a257dba1584a8bd358
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2023-11-22 04:51:41 +0330

    Document middlewares

--- middlewares.go
+++ middlewares.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 )
 
+// LoggerMiddleware logs incoming request's method and URI in `info` level
 func (s *Server) LoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		s.Info("Received request", "method", r.Method, "uri", r.URL.Path)
@@ -11,13 +12,13 @@ func (s *Server) LoggerMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+// RecoverMiddleware will recover from panics. It'll display a log in `error` level with the error message.
 func (s *Server) RecoverMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				s.Error("Panic recovered", "message", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+				s.InternalServerErrorResponse(w)
 			}
 		}()
 		next.ServeHTTP(w, r)
