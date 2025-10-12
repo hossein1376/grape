7819c2256940f240c17ea0484df384e4e560670c
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2025-10-12 18:22:59 +0330

    Expand helper functions to parse param

--- grape.go
+++ grape.go
@@ -14,17 +14,24 @@ type Map = map[string]any
 
 const defaultMaxBodySize = 1_048_576 // 1mb
 
-var ErrMissingParam = errors.New("parameter not found")
+var (
+	ErrMissingParam = errors.New("parameter not found")
+	ErrOverflow     = errors.New("value overflow")
+)
 
 // Param attempts to extract the given parameter from path, or URL query params.
 // Then, the parser function is used to parse it to the expected type.
 // Example of the parser functions for primitive types are: [strconv.Atoi],
-// [strconv.ParseBool], [strconv.ParseFloat], and [ParseInt64]. It can also be
-// manually implemented for custom types.
+// [strconv.ParseBool], [ParseInt], [ParseUint], and [ParseFloat]. It can also
+// be manually implemented for custom types.
+//
+// Examples:
+//
+//	grape.Param(r, "boolean", strconv.ParseBool)
+//	grape.Param(r, "integer", grape.ParseInt[int16]())
+//	grape.Param(r, "float", grape.ParseFloat[float64]())
 func Param[T any](
-	r *http.Request,
-	name string,
-	parser func(s string) (T, error),
+	r *http.Request, name string, parser func(s string) (T, error),
 ) (T, error) {
 	var t T
 	param := r.PathValue(name)
@@ -41,10 +48,52 @@ func Param[T any](
 	return i, nil
 }
 
-// ParseInt64 will attempt to parse the given input into an int64 number with
-// the base of 10. It is meant to be used as an argument to the [Param] func.
-func ParseInt64(s string) (int64, error) {
-	return strconv.ParseInt(s, 10, 64)
+// ParseInt will attempt to parse the given input into an integer of the
+// specified type. It can be used as an argument to the [Param] function.
+func ParseInt[
+	T ~int | ~int64 | ~int32 | ~int16 | ~int8,
+]() func(string) (T, error) {
+	return func(s string) (T, error) {
+		i, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			return 0, err
+		}
+		return checkOverflow[T](s, i)
+	}
+}
+
+// ParseUint will attempt to parse the given input into an unsigned integer of
+// the specified type. It can be used as an argument to the [Param] function.
+func ParseUint[
+	T ~uint | ~uint64 | ~uint32 | ~uint16 | ~uint8,
+]() func(string) (T, error) {
+	return func(s string) (T, error) {
+		i, err := strconv.ParseUint(s, 10, 64)
+		if err != nil {
+			return 0, err
+		}
+		return checkOverflow[T](s, i)
+	}
+}
+
+// ParseFloat will attempt to parse the given input into a floating point number
+// of the specified type. It can be used as an argument to the [Param] function.
+func ParseFloat[T ~float64 | ~float32]() func(string) (T, error) {
+	return func(s string) (T, error) {
+		i, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return 0, err
+		}
+		return checkOverflow[T](s, i)
+	}
+}
+
+func checkOverflow[T any](s string, i any) (T, error) {
+	t := T(i)
+	if fmt.Sprintf("%d", t) != s {
+		return t, ErrOverflow
+	}
+	return t, nil
 }
 
 // Go spawns a new goroutine and will recover in case of panic; logging the
